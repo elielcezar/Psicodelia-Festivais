c69043231c0631d9fa9954c058da8432fd8d039c
Eliel <elielcezar@gmail.com>
2018-07-18 15:33:13 -0300

    mudança no topo dos festivais

--- templates/node--festival.tpl.php
+++ templates/node--festival.tpl.php
@@ -1,59 +1,58 @@
-	
-
-	<div class="header header-filter">
-    	<div class="capa">
-    		<?php print views_embed_view('festival_capa', 'default', $nid); ?>
-    	</div>
-	</div>
 
 
-	<div class="top-content">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8">
-					<?php 
-						global $messages;
-						if($messages){ print $messages; } 
-					?>
-					<h1><?php print $title; ?></h1>
+<div class="top-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<?php
+					global $messages;
+					if($messages){ print $messages; }
+				?>
+				<h1><?php print $title; ?></h1>
 
-					<div class="info-data">
-						<?php print views_embed_view('festival_data', 'block_1', $nid); ?>	
-					</div>	
-
+				<div class="info-data">
+					<?php print views_embed_view('festival_data', 'block_1', $nid); ?>
 				</div>
+
 			</div>
 		</div>
 	</div>
+</div>
 
 <div class="container">
 
 		<div class="row">
 
 			<div class="col-md-8">
-				
-				<div class="conteudo">			
+
+				<div class="conteudo">
 
 					<div class="tab-content">
 
 						<div class="tab-pane active" id="sobre">
 
+							<div class="header header-filter">
+						    	<div class="capa">
+						    		<?php print views_embed_view('festival_capa', 'default', $nid); ?>
+						    	</div>
+							</div>
+
 							<?php if($user->uid){ ?>
 				      			<div class="container-form">
 				      				<?php print views_embed_view('festival_sobre', 'default', $nid); ?>
-				      			</div>			      				
+				      			</div>
 
 				      			<div class="info-mapa">
-									<?php print views_embed_view('festival_data', 'attachment_1', $nid); ?>	
+									<?php print views_embed_view('festival_data', 'attachment_1', $nid); ?>
 								</div>
 							<?php } else { ?>
 									<div class="aviso"><h4><a href="<?php print base_path() ?>user/register" target="_blank">Crie uma conta</a> ou <a href="<?php print base_path() ?>user" target="_blank">faça login</a> para ver todas as informações desse festival.<br/><br/>É rápido e o melhor, de graça! =)</h4></div>
-							<?php } ?>					
+							<?php } ?>
 					      </div>
 
 					</div>
 
-				</div>				
+				</div>
 			</div>
 
 		<div class="col-md-4">
@@ -72,16 +71,16 @@
 			</div>
 
 			<!--div class="banner-2">
-				
+
 			</div-->
-			
+
 		</div>
 
 	</div><!-- row -->
 
 </div><!-- container -->
 
-		
+
 
   <div class="proximos-festivais">
 
@@ -93,18 +92,17 @@
   </div>
 
 
-  
-     
+
+
 
 <script type="text/javascript">
 
 jQuery(document).ready(function ($) {
 
-   var image = $('.views-field-field-imagem-de-capa img').attr('src');
-
-      $('.header-filter .capa').css({ 
+   		/*var image = $('.views-field-field-imagem-de-capa img').attr('src');
+      $('.header-filter .capa').css({
          'background-image': 'url('+ image +')'
-      });  	
+      });*/
 
     /* seleciona o evento no form de resposta */
     /*var node_nid = Drupal.settings.nid;
@@ -148,7 +146,6 @@ jQuery(document).ready(function ($) {
 
  });
 
-	
 
-</script>    
 
+</script>
